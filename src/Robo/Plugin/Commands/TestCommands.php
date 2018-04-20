eb8b0fc50a66e221c90bef32ecdfdcb2adf442be
Balint Kleri <balint@kleri.hu>
2018-04-20 21:27:57 +0200

    Add commands for initializing and running tests

---
+++ src/Robo/Plugin/Commands/TestCommands.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BluesparkLabs\Spark\Robo\Plugin\Commands;
+
+class TestCommands extends \BluesparkLabs\Spark\Robo\Tasks {
+
+  public function testInit() {
+    $this->validateConfig();
+    $this->title('Initiating tests');
+    foreach ($this->config->get('test.init') as $command) {
+      $this->taskSparkExec('containers:exec', ['php', $command]);
+    }
+  }
+
+  public function testExecute() {
+    $this->validateConfig();
+    $this->title('Running tests');
+    foreach ($this->config->get('test.exec') as $command) {
+      $this->taskSparkExec('containers:exec', ['php', $command]);
+    }
+  }
+
+}
