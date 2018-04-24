897b57278e1d2d9ec0a56660c7c3e3f79dba2b68
Balint Kleri <balint@kleri.hu>
2018-04-24 11:07:24 +0200

    Implement db:drop command

--- src/Robo/Plugin/Commands/DbCommands.php
+++ src/Robo/Plugin/Commands/DbCommands.php
@@ -4,6 +4,11 @@ namespace BluesparkLabs\Spark\Robo\Plugin\Commands;
 
 class DbCommands extends \BluesparkLabs\Spark\Robo\Tasks {
 
+  public function dbDrop() {
+    $this->title('Dropping database');
+    $this->taskSparkExec('drush', ['sql-drop -y']);
+  }
+
   public function dbImport() {
     $this->title('Importing database dump');
     $this->taskSparkContainerShExec('db', 'mysql -u root --password=root spark < /opt/spark-project/.spark/db-import.sql');
