f561d0a57b651585f55336d6babf8f75904fa69d
Balint Kleri <balint@kleri.hu>
2018-04-25 20:37:39 +0200

    Implement solr:check-ready and db:check-ready commands

--- src/Robo/Plugin/Commands/DbCommands.php
+++ src/Robo/Plugin/Commands/DbCommands.php
@@ -4,6 +4,12 @@ namespace BluesparkLabs\Spark\Robo\Plugin\Commands;
 
 class DbCommands extends \BluesparkLabs\Spark\Robo\Tasks {
 
+  public function dbCheckReady() {
+    $this->title('Waiting for MariaDB to start');
+    // See https://github.com/wodby/mariadb#orchestration-actions.
+    $this->taskSparkContainerExec('db', 'make check-ready max_try=10 wait_seconds=3 -f /usr/local/bin/actions.mk');
+  }
+
   public function dbDrop() {
     $this->title('Dropping database');
     $this->taskSparkExec('drush', ['sql-drop -y']);
--- src/Robo/Plugin/Commands/SolrCommands.php
+++ src/Robo/Plugin/Commands/SolrCommands.php
@@ -4,6 +4,12 @@ namespace BluesparkLabs\Spark\Robo\Plugin\Commands;
 
 class SolrCommands extends \BluesparkLabs\Spark\Robo\Tasks {
 
+  public function solrCheckReady() {
+    $this->title('Waiting for Solr to start');
+    // See https://github.com/wodby/solr#orchestration-actions.
+    $this->taskSparkContainerExec('solr', 'make check-ready max_try=10 wait_seconds=3 -f /usr/local/bin/actions.mk');
+  }
+
   public function solrInit() {
     $this->title('Creating Solr core');
     $commands = [
