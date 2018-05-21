0f72beb76e20bd9ddaa0247e436aebd53c1cb9d0
Pablo Cerda <pablo@citla.com>
2018-05-21 19:01:44 +0200

    Argument to limit start of container service and documented methods.

--- src/Robo/Plugin/Commands/ContainerCommands.php
+++ src/Robo/Plugin/Commands/ContainerCommands.php
@@ -2,6 +2,8 @@
 
 namespace BluesparkLabs\Spark\Robo\Plugin\Commands;
 
+use Robo\Contract\CommandInterface;
+
 class ContainerCommands extends \BluesparkLabs\Spark\Robo\Tasks {
 
   use \Droath\RoboDockerCompose\Task\loadTasks;
@@ -10,16 +12,30 @@ class ContainerCommands extends \BluesparkLabs\Spark\Robo\Tasks {
     parent::__construct();
   }
 
-  public function containersStart() {
+  /**
+   * Compose all the docker containers.
+   *
+   * @param string $service
+   *   Limit compose to specific container service.
+   */
+  public function containersStart($service = NULL) {
     $this->validateConfig();
     $this->title('Starting containers');
-    $this->taskDockerComposeUp()
-      ->file($this->dockerComposeFile)
+    $command = $this->taskDockerComposeUp();
+
+    if ($service) {
+      $this->limitComposeService($command, $service);
+    }
+
+    $command->file($this->dockerComposeFile)
       ->projectName($this->config->get('name'))
       ->detachedMode()
       ->run();
   }
 
+  /**
+   * Destroy docker containers.
+   */
   public function containersDestroy() {
     $this->validateConfig();
     $this->title('Destroying containers');
@@ -29,6 +45,14 @@ class ContainerCommands extends \BluesparkLabs\Spark\Robo\Tasks {
       ->run();
   }
 
+  /**
+   * Execute a command on a given container.
+   *
+   * @param string $container
+   *   Container service name.
+   * @param string $execute_command
+   *   Command to execute.
+   */
   public function containersExec($container, $execute_command) {
     $this->validateConfig();
     $this->title('Executing in container: ' . $container, FALSE);
@@ -41,9 +65,25 @@ class ContainerCommands extends \BluesparkLabs\Spark\Robo\Tasks {
       ->run();
   }
 
+  /**
+   * Open a SSH connection to specific container.
+   */
   public function containersSsh() {
     $this->title('Logging in to PHP container');
     $this->say('This command is not implemented yet. Copy and execute the following:');
     $this->io()->text('docker-compose --file ./vendor/bluesparklabs/spark/docker/docker-compose.d8.yml --project-name \'Spark Example\' exec php bash');
   }
+
+  /**
+   * Limit composer to specific service.
+   *
+   * @param \Robo\Contract\CommandInterface $command
+   *   Docker compose commmand.
+   * @param string $service
+   *   Service name as defined on docker compose file.
+   */
+  private function limitComposeService(CommandInterface $command, $service) {
+    $command->setService($service);
+  }
+
 }
