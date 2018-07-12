385498f47951f36a66396b2221c6973067fd369a
James Wilson <jrwilson3@gmail.com>
2018-07-12 12:53:46 -0500

    Upload backups to AWS S3 #8

--- src/Robo/Plugin/Commands/DrupalCommands.php
+++ src/Robo/Plugin/Commands/DrupalCommands.php
@@ -2,6 +2,8 @@
 
 namespace BluesparkLabs\Spark\Robo\Plugin\Commands;
 
+use Aws\Exception\AwsException;
+use Aws\S3\S3Client;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -66,6 +68,7 @@ class DrupalCommands extends \BluesparkLabs\Spark\Robo\Tasks {
    *    drupal:
    *      backup:
    *        options:
+   *          bucket: bsp-myproject
    *          truncate: cache,cache_*,sessions,watchdog
    *          skip: migrate_*
    *          files:
@@ -82,7 +85,21 @@ class DrupalCommands extends \BluesparkLabs\Spark\Robo\Tasks {
    * ```
    *
    * @param array $opts
-   * @options $truncate  A comma-separated list of tables from which to
+   * @options $bucket    (Required) The S3 bucket destination for the backup.
+   *                     E.g. 'bsp-myproject'
+   *          $region    The AWS region to connect to. If left blank, the
+   *                     default value is 'us-east-1'. For a list of available
+   *                     regions, see http://bit.ly/s3-regions.
+   *          $profile   The AWS profile to use for connection credentials.
+   *                     Default value is 'default'. The AWS SDK will first
+   *                     try to load credentials from environment variables
+   *                     (http://bit.ly/aws-php-creds). If not found, and if
+   *                     this option is left blank, the SDK then looks for
+   *                     the default credentials in the `~/.aws/credentials`
+   *                     file. Finally, if you specify a custom profile
+   *                     value, the SDK loads credentials from that profile.
+   *                     See http://bit.ly/aws-creds-file for formatting info.
+   *          $truncate  A comma-separated list of tables from which to
    *                     truncate values in the db dump. This maps to the
    *                     drush sql-dump --structure-table-list option.
    *                     Default value is 'cache,cache_*,sessions,watchdog'.
@@ -95,6 +112,9 @@ class DrupalCommands extends \BluesparkLabs\Spark\Robo\Tasks {
    *                     exclude from the tarball.
    */
   public function drupalBackup($opts = [
+    'bucket' => InputOption::VALUE_REQUIRED,
+    'region' => 'us-east-1',
+    'profile' => 'default',
     'truncate' => 'cache,cache_*,watchdog,sessions',
     'skip' => '',
     'files' => null,
@@ -104,14 +124,17 @@ class DrupalCommands extends \BluesparkLabs\Spark\Robo\Tasks {
     // Create unique backup filenames using application name, current
     // environment, and date and time stamp.
     $timestamp = $this->executeDateTime;
-    $name = $this->config->get('name');
+    $application = $this->cleanFileName($this->config->get('name'));
     $environment = getenv('ENVIRONMENT') ?: 'default';
-    $filename = $this->cleanFileName("{$name}-{$environment}-{$timestamp}");
+    $filename = $this->cleanFileName("{$application}-{$environment}-{$timestamp}");
     $dumpfile = "{$filename}.sql";
     $tarfile = "{$filename}.tgz";
 
     $this->title('Executing Drupal backup command');
     $this->taskSparkExec('drush', ["sql-dump --gzip --result-file={$dumpfile} --structure-tables-list={$opts['truncate']} --skip-tables-list={$opts['skip']}"]);
+
+    // The above drush command modifies the dumpfile name upon gzipping.
+    $dumpfile = $dumpfile . '.gz';
 
     // Backup Drupal files folder.
     $this->title('Creating Drupal files tarball');
@@ -146,7 +169,41 @@ class DrupalCommands extends \BluesparkLabs\Spark\Robo\Tasks {
 
     $tarTask->run();
 
-    // @todo implement sync to s3
+    try {
+      // Connect to S3.
+
+      // The following options are required.
+      $s3ClientOptions = [
+        'version' => '2006-03-01',
+        'region' => $opts['region']
+      ];
+      // Add the AWS profile option if provided.
+      if (!empty($opts['profile'])) {
+        $s3ClientOptions['profile'] = $opts['profile'];
+      }
+      $s3Client = new S3Client($s3ClientOptions);
+
+      $this->title("Uploading database to Amazon S3 bucket: {$opts['bucket']}");
+      $result = $s3Client->putObject([
+        'Bucket'     => $opts['bucket'],
+        'Key'        => $application . '/' . $dumpfile,
+        'SourceFile' => $this->webRoot . '/' . $dumpfile,
+      ]);
+      $this->say("Database uploaded to: {$result['ObjectURL']}");
+
+      $this->title("Uploading files tarball to Amazon S3 bucket: {$opts['bucket']}");
+      $this->say(" ... be patient, this may take some time ... ");
+      $this->say(" ... if the operation times out try with --timeout=0 ... ");
+      $result = $s3Client->putObject([
+        'Bucket'     => $opts['bucket'],
+        'Key'        => $application . '/' . $tarfile,
+        'SourceFile' => $this->workDir . '/' . $tarfile,
+      ]);
+      $this->say("Files tarball uploaded to: {$result['ObjectURL']}");
+
+    } catch (S3Exception $e) {
+      $this->say($e->getMessage());
+    }
 
     // @todo implement cleanup s3 dumps older than 15 days
   }
